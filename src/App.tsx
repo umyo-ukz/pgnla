e4f22d54ac4537a5b771cd63c64a7dae86af0875
Nixon <nixonrolle2006@gmail.com>
2026-01-07 14:35:59 -0400

    Path FIXES

--- src/App.tsx
+++ src/App.tsx
@@ -34,59 +34,58 @@ import ComingSoon from "./pages/ComingSoon";
 export default function App() {
   return (
     <LanguageProvider>
-    <BrowserRouter>
-      <div className="min-h-screen flex flex-col">
-        <ScrollToTop />
-        <Navbar />
-        <Routes>
+      <BrowserRouter>
+        <div className="min-h-screen flex flex-col">
+          <ScrollToTop />
+          <Navbar />
+          <Routes>
             <Route path="/" element={<Home />} />
-          {/* Public Routes */}
-          <Route path="/home" element={<Home />} />
-          <Route path="/admissions" element={<Admissions />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/calendar" element={<Calendar />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/financing" element={<Financing />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/registration" element={<Registration />} />
-          <Route path="/coming-soon" element={<ComingSoon />} />
+            {/* Public Routes */}
+            <Route path="/admissions" element={<Admissions />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/calendar" element={<Calendar />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/financing" element={<Financing />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/registration" element={<Registration />} />
+            <Route path="/coming-soon" element={<ComingSoon />} />
 
-          {/* Parent Routes */}
-          <Route path="/parent-dashboard" element={<ParentDashboard />} />
-          <Route path="/student/:studentId/grades" element={<GradesPage />} />
-          <Route path="/parent/student/:studentId" element={<ParentStudentProfile />} />
+            {/* Parent Routes */}
+            <Route path="/parent-dashboard" element={<ParentDashboard />} />
+            <Route path="/student/:studentId/grades" element={<GradesPage />} />
+            <Route path="/parent/student/:studentId" element={<ParentStudentProfile />} />
 
-          {/* Staff Routes - uses StaffLayout with side panel */}
-          <Route path="/staff" element={<StaffLayout />}>
-            <Route index element={<StaffDashboard />} />
-            <Route path="grades" element={<StaffGradesPage />} />
-            <Route path="performance" element={<StudentPerformancePage />} />
-            <Route path="performance/:studentId" element={<StudentProfile />} />
-            <Route path="account" element={<AccountSettings />} />
-          </Route>
+            {/* Staff Routes - uses StaffLayout with side panel */}
+            <Route path="/staff" element={<StaffLayout />}>
+              <Route index element={<StaffDashboard />} />
+              <Route path="grades" element={<StaffGradesPage />} />
+              <Route path="performance" element={<StudentPerformancePage />} />
+              <Route path="performance/:studentId" element={<StudentProfile />} />
+              <Route path="account" element={<AccountSettings />} />
+            </Route>
 
-          {/* Admin Routes - uses AdminLayout with side panel */}
-          <Route path="/admin" element={<AdminLayout />}>
-            <Route index element={<AdminDashboard />} />
-            <Route path="registrations" element={<AdminRegistrations />} />
-            <Route path="registrations/:id" element={<AdminRegistrationDetails />} />
-            <Route path="messages" element={<AdminMessages />} />
-            <Route path="parents" element={<>Parents Page - TODO</>} />
-            <Route path="staff" element={<>Staff Management - TODO</>} />
-            <Route path="settings" element={<>System Settings - TODO</>} />
-            <Route path="account" element={<AccountSettings />} />
-            <Route path="manage-accounts" element={<AdminManageAccounts />} />
-            <Route path="parents/:parentId" element={<AdminParentProfile />} />
+            {/* Admin Routes - uses AdminLayout with side panel */}
+            <Route path="/admin" element={<AdminLayout />}>
+              <Route index element={<AdminDashboard />} />
+              <Route path="registrations" element={<AdminRegistrations />} />
+              <Route path="registrations/:id" element={<AdminRegistrationDetails />} />
+              <Route path="messages" element={<AdminMessages />} />
+              <Route path="parents" element={<>Parents Page - TODO</>} />
+              <Route path="staff" element={<>Staff Management - TODO</>} />
+              <Route path="settings" element={<>System Settings - TODO</>} />
+              <Route path="account" element={<AccountSettings />} />
+              <Route path="manage-accounts" element={<AdminManageAccounts />} />
+              <Route path="parents/:parentId" element={<AdminParentProfile />} />
 
-          </Route>
+            </Route>
 
-          {/* Fallback account route for parents and other roles */}
-          <Route path="/account" element={<AccountSettings />} />
-          <Route path="*" element={<Home />} />
-        </Routes>
-        <Footer />
-      </div>
-    </BrowserRouter>
+            {/* Fallback account route for parents and other roles */}
+            <Route path="/account" element={<AccountSettings />} />
+            <Route path="*" element={<Home />} />
+          </Routes>
+          <Footer />
+        </div>
+      </BrowserRouter>
     </LanguageProvider>
   );
 }
--- src/components/Home/CTA.tsx
+++ src/components/Home/CTA.tsx
@@ -8,11 +8,11 @@ export default function CTA() {
       </h2>
 
       <div className="flex flex-col sm:flex-row gap-4 justify-center">
-        <Link to="admissions"
+        <Link to="/admissions"
           className="btn-primary bg-white text-primary-red hover:bg-gray-100 text-lg px-8 py-4 font-semibold">
           <i className="fas fa-calendar-check mr-2"></i>Apply Now
         </Link>
-        <Link to ="contact"
+        <Link to ="/contact"
           className="btn-secondary border-2 border-white text-white hover:bg-white/20 text-lg px-8 py-4">
           <i className="fas fa-phone mr-2"></i>Contact Us
         </Link>
